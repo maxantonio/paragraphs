9da4883aaa71c856e9dd6d660ba308eade39abe8
Julio Cesar <jcmacias05@gmail.com>
2016-10-02 19:47:49 -0400

    new quotes view

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -69,6 +69,6 @@ class QuotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def quote_params
-      params.fetch(:quote, {})
+      params[:quote].permit(:text_en,:text_es)
     end
 end
